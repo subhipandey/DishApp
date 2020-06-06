a5a32bf5bf1d3fe99c7d35d8c13ae058f4e920e9
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-06 12:36:11 +0530

    Created Room Database for UserLogin

---
+++ app/src/main/java/com/subhipandey/android/dishapp/data/database/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.subhipandey.android.dishapp.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.subhipandey.android.dishapp.data.database.entities.User
+
+@Database(
+    entities = [User::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+
+  abstract fun getUserDao() : UserDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance?:buildDatabase(context).also {
+                instance = it
+            }
+        }
+        private fun buildDatabase(context: Context) =
+        Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "Database.db"
+        ).build()
+    }
+}
---
+++ app/src/main/java/com/subhipandey/android/dishapp/data/database/UserDao.kt
@@ -0,0 +1,19 @@
+package com.subhipandey.android.dishapp.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.subhipandey.android.dishapp.data.database.entities.CURRENT_USER_ID
+import com.subhipandey.android.dishapp.data.database.entities.User
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user: User) : Long
+
+    @Query("SELECT * FROM user WHERE uid = $CURRENT_USER_ID")
+    fun getUser() : LiveData<User>
+}

8143b226d014b864318da44f4aa028c1b10b71cc
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-08 14:41:14 +0530

    DI with Kodein Framework

---
+++ app/src/main/java/com/subhipandey/android/dishapp/DishApplication.kt
@@ -0,0 +1,28 @@
+package com.subhipandey.android.dishapp
+
+import android.app.Application
+import com.subhipandey.android.dishapp.data.database.AppDatabase
+import com.subhipandey.android.dishapp.data.network.Api
+import com.subhipandey.android.dishapp.data.network.NetworkConnectionInterceptor
+import com.subhipandey.android.dishapp.data.repository.UserRepo
+import com.subhipandey.android.dishapp.ui.auth.AuthViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class DishApplication : Application(), KodeinAware {
+
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@DishApplication))
+
+        bind() from singleton { NetworkConnectionInterceptor(instance()) }
+        bind() from singleton {Api(instance())}
+        bind() from singleton { AppDatabase(instance()) }
+        bind() from singleton { UserRepo(instance(), instance())}
+        bind() from provider { AuthViewModelFactory(instance()) }
+    }
+}
--- app/src/main/java/com/subhipandey/android/dishapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/subhipandey/android/dishapp/ui/auth/LoginActivity.kt
@@ -1,38 +1,31 @@
 package com.subhipandey.android.dishapp.ui.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.subhipandey.android.dishapp.R
-import com.subhipandey.android.dishapp.data.database.AppDatabase
 import com.subhipandey.android.dishapp.data.database.entities.User
-import com.subhipandey.android.dishapp.data.network.Api
-import com.subhipandey.android.dishapp.data.network.NetworkConnectionInterceptor
-import com.subhipandey.android.dishapp.data.repository.UserRepo
 import com.subhipandey.android.dishapp.databinding.ActivityLoginBinding
 import com.subhipandey.android.dishapp.ui.home.HomeActivity
 import com.subhipandey.android.dishapp.util.hide
 import com.subhipandey.android.dishapp.util.show
 import com.subhipandey.android.dishapp.util.snackbar
-import com.subhipandey.android.dishapp.util.toast
 import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity(), AuthListener {
+class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: AuthViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
-        val api = Api(networkConnectionInterceptor)
-        val db = AppDatabase(this)
-        val repository = UserRepo(api, db)
-        val factory = AuthViewModelFactory(repository)
 
       val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         val viewModel = ViewModelProviders.of(this, factory).get(AuthViewModel::class.java)

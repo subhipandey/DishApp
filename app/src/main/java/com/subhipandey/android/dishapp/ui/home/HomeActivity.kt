b4c1a9a5940cd163c83fcbea10ee2e26a057fd81
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-09 02:01:58 +0530

    Created Fragment and Navigation for HomeActivity

--- app/src/main/java/com/subhipandey/android/dishapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/subhipandey/android/dishapp/ui/home/HomeActivity.kt
@@ -2,11 +2,28 @@ package com.subhipandey.android.dishapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.subhipandey.android.dishapp.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
     }
-}
+
+        override fun onSupportNavigateUp(): Boolean {
+            return NavigationUI.navigateUp(
+                Navigation.findNavController(this, R.id.fragment),
+                drawer_layout
+            )
+        }
+    }
---
+++ app/src/main/java/com/subhipandey/android/dishapp/util/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.subhipandey.android.dishapp.util.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
